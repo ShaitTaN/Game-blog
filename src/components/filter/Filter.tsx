9f8e3de707451be889571cf03cdb9590fc8bea60
Dmitriy <kdima18@mail.ru>
2022-07-13 23:16:40 +0300

    changed sort for posts page

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,36 +1,74 @@
-import React, { FC, useState } from 'react'
-import './filter.scss'
+import React, { FC, useState } from "react";
+import "./filter.scss";
 
 const Filter: FC = () => {
-	const [dateSort, setDateSort] = useState('dateNew')
-	const [nameSort, setNameSort] = useState('')
+  const [dateSort, setDateSort] = useState("asc");
+  const [nameSort, setNameSort] = useState("asc");
+  const [activeSort, setActiveSort] = useState("date");
 
-	const updateDateSort = (e: any) => {
-		setDateSort(e.target.value)
-	}
-	const updateNameSort = (e: any) => {
-		setNameSort(e.target.value)
-	}
+  const updateDateSort = () => {
+    dateSort === "asc" ? setDateSort("desc") : setDateSort("asc");
+  };
+  const updateNameSort = () => {
+    nameSort === "asc" ? setNameSort("desc") : setNameSort("asc");
+  };
 
-	return (
-		<div className='filter'>
-			<div className="filter__sort">
-				<h2 className='title'>Sort by date</h2>
-				<select name="sort" value={dateSort} onChange={updateDateSort}>
-					<option value="dateNew">new to old</option>
-					<option value="dateOld">old to new</option>
-				</select>
-			</div>
-			<div className="filter__sort">
-				<h2 className='title'>Sort by name</h2>
-				<select name="sort" value={nameSort} onChange={updateNameSort}>
-					<option value="">none</option>
-					<option value="nameA">a-z</option>
-					<option value="nameZ">z-a</option>
-				</select>
-			</div>
-		</div>
-	)
-}
+  const changeSort = (sort: string) => {
+    if (sort === "date") {
+      setActiveSort(sort);
+      updateDateSort();
+      return;
+    }
+    setActiveSort(sort);
+    updateNameSort();
+  };
 
-export default Filter
+  return (
+    <div className="filter">
+      <div
+        className={
+          activeSort === "date" ? "filter__sort active" : "filter__sort"
+        }
+        onClick={() => changeSort("date")}
+      >
+        Sort by date
+        <svg
+          className={
+            dateSort === "asc"
+              ? "filter__sort-arrow"
+              : "filter__sort-arrow active"
+          }
+          width="24"
+          height="24"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        >
+          <path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z" />
+        </svg>
+      </div>
+      <div
+        className={
+          activeSort === "name" ? "filter__sort active" : "filter__sort"
+        }
+        onClick={() => changeSort("name")}
+      >
+        Sort by name
+        <svg
+          className={
+            nameSort === "asc"
+              ? "filter__sort-arrow"
+              : "filter__sort-arrow active"
+          }
+          width="24"
+          height="24"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        >
+          <path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
